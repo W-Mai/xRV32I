c083ff075bed339ed507a42f38176a219f04ef89
W-Mai <1341398182@qq.com>
2022-04-27 04:21:54 +0800

    增加：通用寄存器常量定义，修改部分常量的定义方式

--- src/core/defines.v
+++ src/core/defines.v
@@ -3,12 +3,17 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+`define RegistersNumWidth           5                           // 寄存器个数宽度为5位
+`define InstAddressBusWidth         32                          // 实际指令地址宽度为32位
+`define RegisterAddressBusWidth     1 << `RegistersNumWidth     // 寄存器地址宽度为32位
+`define InstByteWidth               32                          // 实际指令字节宽度为32位
+`define RegistersByteWidth          32                          // 寄存器字节宽度为32位
 
-`define InstAddressBusWidth 32
-`define InstByteWidth       `InstAddressBusWidth/8
+`define InstAddressBus      (`InstAddressBusWidth-1)        :0   // 实际指令地址描述符
+`define InstByteBus         (`InstByteWidth-1)              :0   // 实际指令字节描述符
+`define RegistersAddressBus (`RegisterAddressBusWidth-1)    :0   // 寄存器地址描述符
+`define RegistersByteBus    (`RegistersByteWidth-1)         :0   // 寄存器字节描述符
 
-`define InstAddressBus  (`InstAddressBusWidth-1):0
-`define InstByteBus     (`InstAddressBusWidth-1):0
 
 `define CPURstAddress `InstAddressBusWidth'h0
 
@@ -17,7 +22,7 @@
 `define ZeroWord        32'h0
 `define ZeroReg         5'h0
 
-`define HoldFlagBus     2:0   // 暂停流水总线
+`define HoldFlagBus     2:0        // 暂停流水总线
 `define HoldNone        3'b000     // 不暂停流水总线
 `define HoldPc          3'b001     // 暂停PC
 `define HoldIf          3'b010     // 暂停IF
