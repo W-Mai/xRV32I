4e0531a57c9511a67f9f76b968d51f3cfd2d62ed
W-Mai <1341398182@qq.com>
2022-04-26 18:19:44 +0800

    增加：core_if_id模块

---
+++ src/core/core_if_id.v
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 W-Mai
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+`include "../core/defines.v"
+
+module core_pc_reg(
+	input wire clk,
+	input wire rst,
+
+    output wire[`InstAddressBus]    inst_addr_in,  // 指令地址
+    output wire[`InstByteBus]       inst_in,        // 指令
+
+    output wire[`InstAddressBus]    inst_addr_out,  // 指令地址
+    output wire[`InstByteBus]       inst_out        // 指令
+);
+
+gen_ff #(32) inst_addr_ff	(clk, rst, `CPURstAddress, 	inst_addr_in, 	inst_addr_out);
+
+gen_ff #(32) inst_ff		(clk, rst, `INST_NOP, 		inst_in,		inst_out);
+
+
+endmodule
