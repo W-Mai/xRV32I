b6ddc531902e9c3385ac5951b1b52783cff4bf68
W-Mai <1341398182@qq.com>
2022-04-28 18:01:05 +0800

    增加：ID、ID_EX的绘图

--- docs/framework/frame_config.py
+++ docs/framework/frame_config.py
@@ -35,8 +35,8 @@ class Core_IF_ID(ModelBoxBaseModel):
     Inst_Addr_In = Wire('Inst_Addr(IF, IF_ID)', (0, 31))
     Inst_In = Wire('Int(IF, IF_ID)', (0, 31))
 
-    Inst_Addr_Out = Wire('Inst_Addr_Out', (0, 31))
-    Inst_Out = Wire('Inst_Out', (0, 31))
+    Inst_Out = Wire('Inst(IF_ID, ID)', (0, 31))
+    Inst_Addr_Out = Wire('Inst_Addr(IF_ID, ID)', (0, 31))
 
     class Meta:
         name = 'Core/IF_ID'
@@ -50,21 +50,115 @@ class Core_REGS(ModelBoxBaseModel):
     Write_Addr_In = Wire('Write_Addr_In', (0, 31))
     Write_Data_In = Wire('Write_Data_In', (0, 31))
 
-    Read_Addr1_In = Wire('Read_Addr1_In', (0, 31))
-    Read_Addr2_In = Wire('Read_Addr2_In', (0, 31))
+    Read_Addr1_In = Wire('Reg1_Addr(ID, REGS)', (0, 31))
+    Read_Addr2_In = Wire('Reg2_Addr(ID, REGS)', (0, 31))
 
-    Read_Data1_Out = Wire('Read_Data1_Out', (0, 31))
-    Read_Data2_Out = Wire('Read_Data2_Out', (0, 31))
+    Read_Data1_Out = Wire('Reg1_Data(ID, REGS)', (0, 31))
+    Read_Data2_Out = Wire('Reg2_Data(ID, REGS)', (0, 31))
 
     class Meta:
         name = 'Core/REGS'
 
 
+class Core_ID(ModelBoxBaseModel):
+    RST = Wire('RST')
+
+    Inst_In = Wire('Inst(IF_ID, ID)', (0, 31))
+    Inst_Addr_In = Wire('Inst_Addr(IF_ID, ID)', (0, 31))
+
+    Write_Reg1_Addr_Out = Wire('Reg1_Addr(ID, REGS)', (0, 31))
+    Write_Reg2_Addr_Out = Wire('Reg2_Addr(ID, REGS)', (0, 31))
+
+    Read_Reg1_Data_In = Wire('Reg1_Data(ID, REGS)', (0, 31))
+    Read_Reg2_Data_In = Wire('Reg2_Data(ID, REGS)', (0, 31))
+
+    Inst_Out = Wire('Inst(ID, ID_EX)', (0, 31))
+    Inst_Addr_Out = Wire('Inst_Addr(ID, ID_EX)', (0, 31))
+
+    Reg_We_Out = Wire('Reg_We(ID, ID_EX)')
+    Reg_Write_Addr_Out = Wire('Reg_Write_Addr(ID, ID_EX)', (0, 31))
+    Reg1_Data_Out = Wire('Reg1_Data(ID, ID_EX)', (0, 31))
+    Reg2_Data_Out = Wire('Reg2_Data(ID, ID_EX)', (0, 31))
+
+    Opnum1_Out = Wire('Opnum1(ID, ID_EX)', (0, 31))
+    Opnum2_Out = Wire('Opnum2(ID, ID_EX)', (0, 31))
+
+    Opcode_Out = Wire('Opcode(ID, ID_EX)', (0, 7))
+    Func3_Out = Wire('Func3(ID, ID_EX)', (0, 3))
+    Func7_Out = Wire('Func7(ID, ID_EX)', (0, 7))
+    Rs1_Out = Wire('Rs1(ID, ID_EX)', (0, 4))
+    Rs2_Out = Wire('Rs2(ID, ID_EX)', (0, 4))
+    Rd_Out = Wire('Rd(ID, ID_EX)', (0, 4))
+    ImmI_Out = Wire('ImmI(ID, ID_EX)', (0, 31))
+    ImmS_Out = Wire('ImmS(ID, ID_EX)', (0, 31))
+    ImmB_Out = Wire('ImmB(ID, ID_EX)', (0, 31))
+    ImmU_Out = Wire('ImmU(ID, ID_EX)', (0, 31))
+    ImmJ_Out = Wire('ImmJ(ID, ID_EX)', (0, 31))
+    Shamt_Out = Wire('Shamt(ID, ID_EX)', (0, 4))
+
+    class Meta:
+        name = 'Core/ID'
+
+
+class Core_ID_EX(ModelBoxBaseModel):
+    CLK = Wire('CLK')
+    RST = Wire('RST')
+
+    Inst_In = Wire('Inst(ID, ID_EX)', (0, 31))
+    Inst_Addr_In = Wire('Inst_Addr(ID, ID_EX)', (0, 31))
+    Reg_We_In = Wire('Reg_We(ID, ID_EX)')
+    Reg_Write_Addr_In = Wire('Reg_Write_Addr(ID, ID_EX)', (0, 31))
+    Reg1_Data_In = Wire('Reg1_Data(ID, ID_EX)', (0, 31))
+    Reg2_Data_In = Wire('Reg2_Data(ID, ID_EX)', (0, 31))
+    Opnum1_In = Wire('Opnum1(ID, ID_EX)', (0, 31))
+    Opnum2_In = Wire('Opnum2(ID, ID_EX)', (0, 31))
+    Opcode_In = Wire('Opcode(ID, ID_EX)', (0, 7))
+    Func3_In = Wire('Func3(ID, ID_EX)', (0, 3))
+    Func7_In = Wire('Func7(ID, ID_EX)', (0, 7))
+    Rs1_In = Wire('Rs1(ID, ID_EX)', (0, 4))
+    Rs2_In = Wire('Rs2(ID, ID_EX)', (0, 4))
+    Rd_In = Wire('Rd(ID, ID_EX)', (0, 4))
+    ImmI_In = Wire('ImmI(ID, ID_EX)', (0, 31))
+    ImmS_In = Wire('ImmS(ID, ID_EX)', (0, 31))
+    ImmB_In = Wire('ImmB(ID, ID_EX)', (0, 31))
+    ImmU_In = Wire('ImmU(ID, ID_EX)', (0, 31))
+    ImmJ_In = Wire('ImmJ(ID, ID_EX)', (0, 31))
+    Shamt_In = Wire('Shamt(ID, ID_EX)', (0, 4))
+
+    ############################################################################
+
+    Inst_Out = Wire('Inst(ID_EX, EX)', (0, 31))
+    Inst_Addr_Out = Wire('Inst_Addr(ID_EX, EX)', (0, 31))
+    Reg_We_Out = Wire('Reg_We_Out')
+    Reg_Write_Addr_Out = Wire('Reg_Write_Addr_Out', (0, 31))
+    Reg1_Data_Out = Wire('Reg1_Data_Out', (0, 31))
+    Reg2_Data_Out = Wire('Reg2_Data_Out', (0, 31))
+    Opnum1_Out = Wire('Opnum1_Out', (0, 31))
+    Opnum2_Out = Wire('Opnum2_Out', (0, 31))
+    Opcode_Out = Wire('Opcode_Out', (0, 7))
+    Func3_Out = Wire('Func3_Out', (0, 3))
+    Func7_Out = Wire('Func7_Out', (0, 7))
+    Rs1_Out = Wire('Rs1_Out', (0, 4))
+    Rs2_Out = Wire('Rs2_Out', (0, 4))
+    Rd_Out = Wire('Rd_Out', (0, 4))
+    ImmI_Out = Wire('ImmI_Out', (0, 31))
+    ImmS_Out = Wire('ImmS_Out', (0, 31))
+    ImmB_Out = Wire('ImmB_Out', (0, 31))
+    ImmU_Out = Wire('ImmU_Out', (0, 31))
+    ImmJ_Out = Wire('ImmJ_Out', (0, 31))
+    Shamt_Out = Wire('Shamt_Out', (0, 4))
+
+    class Meta:
+        name = 'Core/ID_EX'
+
+
 CONFIG = CONFIGURE(
     node_pos_pair={
         Core_PC_REG: (-10, 0),
-        Core_IF: (0, -5),
-        Core_IF_ID: (10, 0),
-        Core_REGS: (0, 10),
+        Core_IF: (0, -3),
+        Core_IF_ID: (10, 8.6),
+        Core_REGS: (0, 8),
+        Core_ID: (20, 6.8),
+        Core_ID_EX: (38, 10),
     }, other_conf={}
 )
