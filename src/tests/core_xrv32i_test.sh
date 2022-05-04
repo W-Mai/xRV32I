313e0dc50641c2923fb69afbb96808565aa80b2f
W-Mai <1341398182@qq.com>
2022-05-05 00:59:44 +0800

    测试：增加add测试代码（测试失败）
    但是没有测试通过，pc没有自增，下一步排查原因

--- src/tests/core_xrv32i_test.sh
+++ src/tests/core_xrv32i_test.sh
@@ -3,9 +3,6 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-# iverilog -o core_xrv32i_test.vvp core_xrv32i_test.v
-# vvp core_xrv32i_test.vvp
-
 iverilog -o core_xrv32i_test.vvp \
                                 ../utils/gen_ff.v \
                                 ../core/core_pc_reg.v \
@@ -16,4 +13,6 @@ iverilog -o core_xrv32i_test.vvp \
                                 ../core/core_id_ex.v \
                                 ../core/core_alu.v \
                                 ../core/core_ex.v \
-                                ../core/xrv32i.v
+                                core_xrv32i_test.v
+
+vvp core_xrv32i_test.vvp
--- src/tests/core_xrv32i_test.v
+++ src/tests/core_xrv32i_test.v
@@ -7,17 +7,70 @@
 `timescale 1ns/1ns
 
 `include "../core/defines.v"
+`include "../core/xrv32i.v"
 
 module pc_reg_test();
 
+reg clk								;
+reg rst								;
+wire[`InstByteBus]    inst      	;
+wire[`MemAddressBus]  inst_addr		;
+
+reg[`InstByteBus]	  insts[0:3]     ;
+
+assign inst = insts[inst_addr];
+
+xrv32i xrv32i_inst(
+    .clk(clk),
+    .rst(rst),
+
+    .inst_in         (inst), // 指令输入
+    .inst_addr_out   (inst_addr)  // 指令地址输出
+);
+
+wire[`RegistersByteBus] r27, r28, r29;
+
+assign r27 = xrv32i_inst.regs_inst.regs[27];
+assign r28 = xrv32i_inst.regs_inst.regs[28];
+assign r29 = xrv32i_inst.regs_inst.regs[29];
+
 	always begin
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+		clk = 1'b0; #10; clk = 1'b1; #10 $display("r27:%d r28:%d r29:%d", r27, r28, r29);
+
 
 		$finish;
 	end
 
 	initial begin
+        rst = `RstEnable; #10;
+        rst = `RstDisable; #10;
+
 		$dumpfile("wave.vcd"); // 指定用作dumpfile的文件
 		$dumpvars; // dump all vars
+
+        $readmemb("rom_data_bin.dat", insts); // 读取rom_data.dat中的数据到insts数组中
 	end
 
 endmodule
