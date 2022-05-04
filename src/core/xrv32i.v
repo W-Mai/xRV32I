5ed376c629c497d8cbf3f4b5a9eaa77bfa5072c0
W-Mai <1341398182@qq.com>
2022-05-04 17:32:50 +0800

    gitflow-feature-stash: core_riscv

---
+++ src/core/xrv32i.v
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 W-Mai
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+`include "../core/defines.v"
+
+module xrv32i(
+    input clk,
+    input rst,
+
+    input [`InstByteBus]    inst_in,        // 指令输入
+
+    output [`MemAddressBus] inst_addr_out,  // 指令地址输出
+);
+
+
+
+endmodule
