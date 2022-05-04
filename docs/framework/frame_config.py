9f07b6d4c5f90bd13450a160d77e33f829960fcc
W-Mai <1341398182@qq.com>
2022-05-05 02:47:44 +0800

    增加：xRV32I的绘图

--- docs/framework/frame_config.py
+++ docs/framework/frame_config.py
@@ -22,8 +22,8 @@ class Core_PC_REG(ModelBoxBaseModel):
 class Core_IF(ModelBoxBaseModel):
     PC_Addr_In = Wire('PC(PC_REG, IF)', (0, 31))
 
-    ROM_Addr_Out = Wire('ROM_Addr_Out', (0, 31))
-    ROM_Data_In = Wire('ROM_Data_In', (0, 31))
+    ROM_Addr_Out = Wire('Inst_Addr(IF, TOP)', (0, 31))
+    ROM_Data_In = Wire('Inst(TOP, IF)', (0, 31))
 
     Inst_Addr_Out = Wire('Inst_Addr(IF, IF_ID)', (0, 31))
     Int_Out = Wire('Int(IF, IF_ID)', (0, 31))
@@ -219,20 +219,33 @@ class Core_EX(ModelBoxBaseModel):
         name = 'Core/EX'
 
 
+class xRV32I(ModelBoxBaseModel):
+    CLK = Wire('CLK')
+    RST = Wire('RST')
+
+    Inst_Addr_Out = Wire('Inst_Addr(IF, TOP)', (0, 31))
+    Inst_In = Wire('Inst(TOP, IF)', (0, 31))
+
+    class Meta:
+        name = 'xRV32I'
+
+
 CONFIG = CONFIGURE(
     node_pos_pair={
-        Core_PC_REG: (-10, 0),
-        Core_IF: (0, -3),
-        Core_IF_ID: (10, 8.6),
-        Core_REGS: (0, 8),
+        Core_PC_REG: (-6, 0),
+        Core_IF: (2, -3),
+        Core_IF_ID: (12, 8.6),
+        Core_REGS: (2, 8),
         Core_ID: (20, 6.8),
-        Core_ID_EX: (38, 12),
-        Core_ALU: (44, 7.8),
-        Core_EX: (56, 12),
+        Core_ID_EX: (34, 12),
+        Core_ALU: (40.5, 7.8),
+        Core_EX: (52, 12),
+
+        xRV32I: (-16, 5),
     }, connector_pair={
-        Connector(Core_EX.Reg_We_Out, Core_REGS.Write_En_In, (56, 14), (-3, 14)),
-        Connector(Core_EX.Reg_Write_Addr_Out, Core_REGS.Write_Addr_In, (56, 14.25), (-3, 14.25)),
-        Connector(Core_EX.Reg_Write_Data_Out, Core_REGS.Write_Data_In, (56, 14.5), (-3, 14.5)),
+        Connector(Core_EX.Reg_We_Out, Core_REGS.Write_En_In, (56, 14), (-1, 14)),
+        Connector(Core_EX.Reg_Write_Addr_Out, Core_REGS.Write_Addr_In, (56, 14.25), (-1, 14.25)),
+        Connector(Core_EX.Reg_Write_Data_Out, Core_REGS.Write_Data_In, (56, 14.5), (-1, 14.5)),
     }, other_conf={}
     , colors=[color_hex(*hsl_to_rgb(h, 0.5, 0.5)) for h in [0 + i * 1 / 32 for i in range(32)]]
 )
