93aec13019743da174f59a4189ef0223018784c5
W-Mai <1341398182@qq.com>
2022-05-06 23:23:01 +0800

    增加：流水线暂停相关常量

--- src/core/defines.v
+++ src/core/defines.v
@@ -33,14 +33,17 @@
 `define OneWord         32'h1
 `define ZeroReg         5'h0
 
-`define HoldFlagBus     2:0        // 暂停流水总线
-`define HoldNone        3'b000     // 不暂停流水总线
-`define HoldPc          3'b001     // 暂停PC
-`define HoldIf          3'b010     // 暂停IF
-`define HoldId          3'b011     // 暂停ID
+`define HoldFlagBus     2:0         // 暂停流水总线
+`define HoldNone        3'b000      // 不暂停流水总线
+`define HoldPc          3'b001      // 暂停PC
+`define HoldIf          3'b010      // 暂停IF
+`define HoldId          3'b011      // 暂停ID
 
-`define JumpEnable  1'b1
-`define JumpDisable 1'b0
+`define HoldEnable      1'b1        // 暂停使能
+`define HoldDisable     1'b0        // 暂停禁止
+
+`define JumpEnable      1'b1        // 跳转使能
+`define JumpDisable     1'b0        // 跳转禁止
 
 `define INST_OPCODEWidth    7
 `define INST_REGBusWidth    `RegisterAddressBusWidth
