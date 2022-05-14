436cd135ee2091262c815d1afe6f6e8404739a98
W-Mai <1341398182@qq.com>
2022-05-14 14:33:28 +0800

    重构：替换常量

--- src/core/xSimBus.v
+++ src/core/xSimBus.v
@@ -9,19 +9,19 @@ module xSimBus(
     input clk,
     input rst,
 
-    input wire[31:0]        devices_in         , // 设备请求输入， 用{}聚合，每个设备一个bit，0表示不请求，1表示请求
+    input wire[`XSimBusDeviceNum-1:0]       devices_in         , // 设备请求输入， 用{}聚合，每个设备一个bit，0表示不请求，1表示请求
 
     // 选中哪台设备了
-    output reg[4:0]         master_id_out      , // 主设备ID
-    output reg[4:0]         device_id_out      , // 从设备ID
+    output reg[`XSimBusDeviceBus]           master_id_out      , // 主设备ID
+    output reg[`XSimBusDeviceBus]           device_id_out      , // 从设备ID
 
-    output reg[26:0]        device_addr_out    , // 从设备地址, 绑定所有设备的地址总线
+    output reg[`XSimBusDeviceAddressBus]    device_addr_out    , // 从设备地址, 绑定所有设备的地址总线
 
-    input  wire[31:0]       addr_in            , // 访问地址
-    input  wire[31:0]       data_in            , // 总线数据输入
-    output reg[31:0]        data_out           , // 总线数据输出
+    input  wire[`MemAddressBus]             addr_in            , // 访问地址
+    input  wire[`MemByteBus]                data_in            , // 总线数据输入
+    output reg[`MemByteBus]                 data_out           , // 总线数据输出
 
-    output reg              hold_flag_out        // 是否持有总线
+    output reg                              hold_flag_out        // 是否持有总线
 );
 
 wire[4:0]  master_id;
@@ -43,8 +43,8 @@ always @(posedge clk) begin
         hold_flag_out   <= `HoldDisable;
     end else begin
         data_out        <= data_in;
-        device_addr_out <= addr_in[26:0];
-        device_id_out   <= addr_in[31:27];
+        device_addr_out <= addr_in[`XSimBusDeviceAddressBus];
+        device_id_out   <= addr_in[`XSimBusDeviceNum-1:`XSimBusDeviceAddressWidth];
         master_id_out   <= master_id;
 
         hold_flag_out   <= master_id == 31 ? `HoldDisable : `HoldEnable;
